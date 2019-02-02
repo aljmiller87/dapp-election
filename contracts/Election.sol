c55b2c35c8e0c09b86a567d774413de0e617094e
Alex Miller <alexmiller@Alex.attlocal.net>
2019-02-02 10:55:12 -0500

    Election smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+    // Store Candidate
+    // Read candidate
+    // Constructor
+
+    string public candidate;
+
+    constructor () public {
+        candidate = "Candidate 1";
+    }
+
+}
